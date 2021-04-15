8593a945c16407b566190aeb9b350013f08aad2a
K.R.Schuurman <57346737+KRS-dev@users.noreply.github.com>
2021-04-15 15:32:34 +0200

    Update finetune.py

--- finetune.py
+++ finetune.py
@@ -10,31 +10,32 @@ import torch.optim as optim
 import torch.utils.data
 from torch.autograd import Variable
 import torch.nn.functional as F
-# import skimage
-# import skimage.io
-# import skimage.transform
+import skimage
+import skimage.io
+import skimage.transform
 import numpy as np
 import time
 import math
-from dataloader import listflowfile as lt
-from dataloader import SecenFlowLoader as DA
+from dataloader import KITTILoader as DA
 
 from models import *
+
+print("version 1.3")
 
 parser = argparse.ArgumentParser(description='PSMNet')
 parser.add_argument('--maxdisp', type=int, default=192,
                     help='maxium disparity')
 parser.add_argument('--model', default='stackhourglass',
                     help='select model')
-parser.add_argument('--datatype', default='2015',
+parser.add_argument('--datatype', default='2012',
                     help='datapath')
-parser.add_argument('--datapath', default=r"E:/TU-delft/CS4240 - Deep Learning/PSMNet/dataset/sceneflow flyingthings3d/" ,
+parser.add_argument('--datapath', default='./data_kitti/training/',
                     help='datapath')
-parser.add_argument('--epochs', type=int, default=1,
+parser.add_argument('--epochs', type=int, default=300,
                     help='number of epochs to train')
-parser.add_argument('--loadmodel', default=r'E:\TU-delft\CS4240 - Deep Learning\PSMNet\trained\pretrained_model_KITTI2015.tar',
+parser.add_argument('--loadmodel', default='./pretrained_model_KITTI2012.tar',
                     help='load model')
-parser.add_argument('--savemodel', default='./training/',
+parser.add_argument('--savemodel', default='./',
                     help='save model')
 parser.add_argument('--no-cuda', action='store_true', default=False,
                     help='enables CUDA training')
@@ -51,17 +52,15 @@ if args.datatype == '2015':
 elif args.datatype == '2012':
     from dataloader import KITTIloader2012 as ls
 
-all_left_img, all_right_img, all_left_disp, test_left_img, test_right_img, test_left_disp = lt.dataloader(
-    args.datapath)
+all_left_img, all_right_img, all_left_disp, test_left_img, test_right_img, test_left_disp = ls.dataloader(args.datapath)
 
 TrainImgLoader = torch.utils.data.DataLoader(
-         DA.myImageFloder(all_left_img, all_right_img, all_left_disp, True),
-    batch_size=1, shuffle=True, num_workers=1, drop_last=False)
+    DA.myImageFloder(all_left_img, all_right_img, all_left_disp, True),
+    batch_size=4, shuffle=True, num_workers=2, drop_last=False) #12 worker 8
 
 TestImgLoader = torch.utils.data.DataLoader(
-         DA.myImageFloder(test_left_img, test_right_img,
-                          test_left_disp, False),
-    batch_size=1, shuffle=False, num_workers=1, drop_last=False)
+    DA.myImageFloder(test_left_img, test_right_img, test_left_disp, False),
+    batch_size=4, shuffle=False, num_workers=2, drop_last=False) #8 worker 4
 
 if args.model == 'stackhourglass':
     model = stackhourglass(args.maxdisp)
@@ -78,8 +77,7 @@ if args.loadmodel is not None:
     state_dict = torch.load(args.loadmodel)
     model.load_state_dict(state_dict['state_dict'])
 
-print('Number of model parameters: {}'.format(
-    sum([p.data.nelement() for p in model.parameters()])))
+print('Number of model parameters: {}'.format(sum([p.data.nelement() for p in model.parameters()])))
 
 optimizer = optim.Adam(model.parameters(), lr=0.1, betas=(0.9, 0.999))
 
@@ -105,13 +103,13 @@ def train(imgL, imgR, disp_L):
         output1 = torch.squeeze(output1, 1)
         output2 = torch.squeeze(output2, 1)
         output3 = torch.squeeze(output3, 1)
-        loss = 0.5*F.smooth_l1_loss(output1[mask], disp_true[mask], size_average=True) + 0.7*F.smooth_l1_loss(
-            output2[mask], disp_true[mask], size_average=True) + F.smooth_l1_loss(output3[mask], disp_true[mask], size_average=True)
+        loss = 0.5 * F.smooth_l1_loss(output1[mask], disp_true[mask], size_average=True) + 0.7 * F.smooth_l1_loss(
+            output2[mask], disp_true[mask], size_average=True) + F.smooth_l1_loss(output3[mask], disp_true[mask],
+                                                                                  size_average=True)
     elif args.model == 'basic':
         output = model(imgL, imgR)
-        output = torch.squeeze(output3, 1)
-        loss = F.smooth_l1_loss(
-            output3[mask], disp_true[mask], size_average=True)
+        output = torch.squeeze(output, 1)
+        loss = F.smooth_l1_loss(output[mask], disp_true[mask], size_average=True)
 
     loss.backward()
     optimizer.step()
@@ -130,17 +128,19 @@ def test(imgL, imgR, disp_true):
         output3 = model(imgL, imgR)
 
     pred_disp = output3.data.cpu()
-
-    #computing 3-px error#
+    pred_disp = torch.squeeze(pred_disp)
+    # computing 3-px error#
     true_disp = disp_true
+    true_disp = torch.squeeze(true_disp)
     index = np.argwhere(true_disp > 0)
     disp_true[index[0][:], index[1][:], index[2][:]] = np.abs(
-        true_disp[index[0][:], index[1][:], index[2][:]]-pred_disp[index[0][:], index[1][:], index[2][:]])
+        true_disp[index[0][:], index[1][:], index[2][:]] - pred_disp[index[0][:], index[1][:], index[2][:]])
     correct = (disp_true[index[0][:], index[1][:], index[2][:]] < 3) | (
-        disp_true[index[0][:], index[1][:], index[2][:]] < true_disp[index[0][:], index[1][:], index[2][:]]*0.05)
+                disp_true[index[0][:], index[1][:], index[2][:]] < true_disp[
+            index[0][:], index[1][:], index[2][:]] * 0.05)
     torch.cuda.empty_cache()
 
-    return 1-(float(torch.sum(correct))/float(len(index[0])))
+    return 1 - (float(torch.sum(correct)) / float(len(index[0])))
 
 
 def adjust_learning_rate(optimizer, epoch):
@@ -157,16 +157,13 @@ def main():
     max_acc = 0
     max_epo = 0
     start_full_time = time.time()
-
-    for epoch in range(1, args.epochs+1):
+    for epoch in range(1, args.epochs + 1):
         total_train_loss = 0
         total_test_loss = 0
         adjust_learning_rate(optimizer, epoch)
-
         ## training ##
         for batch_idx, (imgL_crop, imgR_crop, disp_crop_L) in enumerate(TrainImgLoader):
             start_time = time.time()
-
             loss = train(imgL_crop, imgR_crop, disp_crop_L)
             print('Iter %d training loss = %.3f , time = %.2f' %
                     (batch_idx, loss, time.time() - start_time))
